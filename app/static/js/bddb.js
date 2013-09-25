2b3dc74a39e70842d12bea17aaccaa7764112d03
kkarra <kkarra@stanford.edu>
2013-09-25 15:45:15 -0700

    eb changes

--- app/static/js/bddb.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	
-	geoUrl = "http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=";
-	sraUrl = "ww.ncbi.nlm.nih.gov/sra?term=;"
-	pubmedUrl = "http://www.ncbi.nlm.nih.gov/pubmed/";
-
-	
-});  // end document ready
-	
